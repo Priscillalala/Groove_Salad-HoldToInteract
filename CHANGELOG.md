f9ccf6ce7c8fdaa986c675d752a5cc19cf104bc9
Priscillalala <95263024+Priscillalala@users.noreply.github.com>
2024-03-16 16:15:55 -0700

    update config system

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 ## 1.1.0
 * Fixed some error spam
 * Added `reset_delay_per_interactable` config option (enabled by default)
+    * Update config system to support injecting new fields
 
 ## 1.0.0
 * First release
--- main.lua
+++ main.lua
@@ -1,13 +1,36 @@
-local configPath = path.combine(paths.config(), "Groove_Salad-HoldToInteract.cfg")
+local config_path = path.combine(paths.config(), "Groove_Salad-HoldToInteract.cfg")
 
-local exists, config = pcall(toml.decodeFromFile, configPath, { formattedIntsAsUserData = false })
-if not exists then
-    log.info("Generating config file")
-    config = {
-        interaction_delay = 0.25,
-        reset_delay_per_interactable = true,
-    }
-    pcall(toml.encodeToFile, config, configPath)
+local default_config = {
+   interaction_delay = 0.25,
+   reset_delay_per_interactable = true,
+}
+local update_config = false
+
+local exists, config = pcall(toml.decodeFromFile, config_path, { formattedIntsAsUserData = false })
+if exists then
+   ---@param config_table table
+   ---@param default_config_table table
+   local function check_update_config(config_table, default_config_table)
+      for key, default_value in pairs(default_config_table) do
+         local value_type = type(default_value)
+         local config_value = config_table[key]
+         if type(config_value) ~= value_type then
+            config_table[key] = default_value
+            update_config = true
+         elseif value_type == "table" then
+            check_update_config(config_value, default_value)
+         end
+      end
+   end
+   check_update_config(config, default_config)
+else
+   config = default_config
+   update_config = true
+end
+
+if update_config then
+   log.info("Updating config file")
+   pcall(toml.encodeToFile, config, { file = config_path, overwrite = true })
 end
 
 local last_interaction_time = 0.0
